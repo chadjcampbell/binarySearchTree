9fcb62510d91ff79c9e7008dada174b1016b721c
Chad <chadjcampbell@gmail.com>
2022-10-06 13:16:40 -0400

    setup Node and Tree class, test buildTree method

---
+++ binarySearchTree.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+    this.root = this.buildTree(arr);
+  }
+
+  buildTree(arr) {
+    let start = 0;
+    let end = arr.length - 1;
+    if (start > end) {
+      return null;
+    }
+    let mid = Math.floor(arr.length / 2);
+    let node = new Node(arr[mid]);
+    node.left = this.buildTree(arr.slice(start, mid));
+    node.right = this.buildTree(arr.slice(mid + 1));
+    return node;
+  }
+}
+
+/* function preOrder(node) {
+  if (node == null) {
+    return;
+  }
+  document.write(node.data + " ");
+  preOrder(node.left);
+  preOrder(node.right);
+} */
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+let binaryTree = new Tree(arr);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(binaryTree.root);
